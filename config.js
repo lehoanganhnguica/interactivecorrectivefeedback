ed62acb445f375a2b9536dc1e82bd8b324cfed21
lehoanganhnguica <lehoang.anhnguica@gmail.com>
2026-06-05 13:18:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.WFS_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: ""
+  supabaseUrl: "https://cuatbmodjeracmxkjjll.supabase.co",
+  supabaseAnonKey: "sb_publishable_ND-pzeZ1Us_F_h6KufNFcA_UD50uyVL"
 };
